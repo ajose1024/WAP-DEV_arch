24c29276ed45509a453b0ec5a4bad661d00cd6a8
ajose1024 <ajose.gomes@ajgomes.datanet-pt.net>
2013-09-14 14:14:10 +0100

    2013.09.14

---
+++ App_data/resources/scripts/data_context/1234567890/app-start_data.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var init_app_context = { 'act_ctx_id':  'abcdefghijklmnopqrstuvwxyz012345' ,
+                         'in_ctx_id':   '6789abcdefghijklmnopqrstuvwxyz01' ,
+                         'sadr_ctx_id': '23456789abcdefghijklmnopqrstuvwx' ,
+                         'mq_ctx_id':   'yz0123456789abcdefghijklmnopqrst' ,
+                         'mem_ctx_id':  'uvwxyz0123456789abcdefghijklmnop' ,
+                         'res_ctx_id':  'qrstuvwxyz0123456789abcdefghijkl' ,
+                         'form_ctx_id': 'mnopqrstuvwxyz0123456789abcdefgh' ,
+                         
+                         'app_vers':    '0.001'
+                       }
+
+
